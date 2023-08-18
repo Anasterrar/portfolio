c1d871297994d0c2379fc43980929d50663077b0
Anasterrar <anas.terrar@gmail.com>
2023-08-18 11:12:19 +0200

    Base

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navigation from './Components/Header/Navigation';
+
+
+function App() {
+  return (
+    <div className="App">
+           <Navigation />
+    </div>
+  );
+}
+
+export default App;
